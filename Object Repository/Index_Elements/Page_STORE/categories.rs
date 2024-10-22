f9a97c0a907377ca1614ff7d4fe046e72c68221f
esteban <juangomez1947@outlook.com>
2024-10-22 17:03:15 -0500

    catagories and products automation

---
+++ Object Repository/Index_Elements/Page_STORE/categories.rs
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>categories</name>
+   <tag></tag>
+   <elementGuidId>c27e22bd-d4ad-4411-8e47-c4fef73d17bf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.col-lg-3</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='contcont']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>div >> internal:has-text=&quot;CATEGORIES Phones Laptops Monitors&quot;i >> nth=2</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>e171708b-de5b-4b10-87fe-98cd2f44a107</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-lg-3</value>
+      <webElementGuid>7bc07e29-19f1-4e88-b0c7-8d4d7b0f7bd9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        
+          CATEGORIES
+          Phones
+          Laptops
+          Monitors
+        
+      </value>
+      <webElementGuid>70c32842-d48d-425c-b8d1-0e16fb3759dd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;contcont&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-3&quot;]</value>
+      <webElementGuid>9f65858e-3787-446c-a554-316d63ccaa80</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='contcont']/div/div</value>
+      <webElementGuid>8c946311-7707-4603-b831-d53684d22c2a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Next'])[1]/following::div[3]</value>
+      <webElementGuid>bf55c70d-9a3b-4dbc-800d-445c98ecf675</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Previous'])[1]/following::div[3]</value>
+      <webElementGuid>17f48ea2-96dd-42d5-9be2-395d3bac2482</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Samsung galaxy s6'])[1]/preceding::div[2]</value>
+      <webElementGuid>4ce2c766-2a5d-4f6b-9d48-e042db96a65a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/div/div</value>
+      <webElementGuid>6e1916b2-c313-4b0f-8339-d1091776f5cf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+        
+          CATEGORIES
+          Phones
+          Laptops
+          Monitors
+        
+      ' or . = '
+        
+          CATEGORIES
+          Phones
+          Laptops
+          Monitors
+        
+      ')]</value>
+      <webElementGuid>1def3707-832e-435d-b821-7b3fcb4c61b0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Index_Elements/Page_STORE/elements.rs
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>elements</name>
+   <tag></tag>
+   <elementGuidId>6cbd20a5-2b31-4632-be50-2c8ee0a792fe</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#tbodyid</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='tbodyid']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#tbodyid</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>1993622b-1498-493d-8723-3f3b9f794b2d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>row</value>
+      <webElementGuid>3d26cd2f-bc1e-4095-8149-d9784dc65914</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>tbodyid</value>
+      <webElementGuid>85678928-91e8-4a1a-a027-4c258388dd31</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        Samsung galaxy s6$360The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420
+ processor and it comes with 3GB of RAM. The phone packs 32GB of 
+internal storage cannot be expanded.   Nokia lumia 1520$820The Nokia Lumia 1520 is powered by 2.2GHz quad-core Qualcomm Snapdragon 800 processor and it comes with 2GB of RAM.   Nexus 6$650The Motorola Google Nexus 6 is powered by 2.7GHz quad-core Qualcomm Snapdragon 805 processor and it comes with 3GB of RAM.  Samsung galaxy s7$800The Samsung Galaxy S7 is powered by 1.6GHz octa-core it comes with 4GB 
+of RAM. The phone packs 32GB of internal storage that can be expanded up
+ to 200GB via a microSD card.  Iphone 6 32gb$790It comes with 1GB of RAM. The phone packs 16GB of internal storage 
+cannot be expanded. As far as the cameras are concerned, the Apple 
+iPhone 6 packs a 8-megapixel primary camera on the rear and a 
+1.2-megapixel front shooter for selfies.  Sony xperia z5$320Sony Xperia Z5 Dual smartphone was launched in September 2015. The phone
+ comes with a 5.20-inch touchscreen display with a resolution of 1080 
+pixels by 1920 pixels at a PPI of 424 pixels per inch.  HTC One M9$700The HTC One M9 is powered by 1.5GHz octa-core Qualcomm Snapdragon 810 
+processor and it comes with 3GB of RAM. The phone packs 32GB of internal
+ storage that can be expanded up to 128GB via a microSD card.   Sony vaio i5$790Sony is so confident that the VAIO S is a superior ultraportable laptop 
+that the company proudly compares the notebook to Apple's 13-inch 
+MacBook Pro. And in a lot of ways this notebook is better, thanks to a 
+lighter weight.  Sony vaio i7
+$790REVIEW
+ 
+Sony is so confident that the VAIO S is a superior 
+ultraportable laptop that the company proudly compares the notebook to 
+Apple's 13-inch MacBook Pro. And in a lot of ways this notebook is 
+better, thanks to a lighter weight, higher-resolution display, more 
+storage space, and a Blu-ray drive.   </value>
+      <webElementGuid>a08ef868-c77c-4bff-9de4-17f4ead26269</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;tbodyid&quot;)</value>
+      <webElementGuid>fec5b1ea-3231-4cf6-b05e-1c3ba5016328</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='tbodyid']</value>
+      <webElementGuid>6758a3d6-14df-47e0-ac71-353364fba321</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='contcont']/div/div[2]/div</value>
+      <webElementGuid>85b3e7e6-fc1e-498a-b16d-0640649bcc0a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='CATEGORIES'])[1]/following::div[2]</value>
+      <webElementGuid>c95a5f91-38ce-4520-a5ab-d177150741dd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Next'])[1]/following::div[6]</value>
+      <webElementGuid>9a29e654-610c-4e4a-aa9d-5b1e36b20a41</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/div/div[2]/div</value>
+      <webElementGuid>3ccba7c4-4dea-4b2c-8ec7-0a9f1f95944a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'tbodyid' and (text() = concat(&quot;
+        Samsung galaxy s6$360The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420
+ processor and it comes with 3GB of RAM. The phone packs 32GB of 
+internal storage cannot be expanded.   Nokia lumia 1520$820The Nokia Lumia 1520 is powered by 2.2GHz quad-core Qualcomm Snapdragon 800 processor and it comes with 2GB of RAM.   Nexus 6$650The Motorola Google Nexus 6 is powered by 2.7GHz quad-core Qualcomm Snapdragon 805 processor and it comes with 3GB of RAM.  Samsung galaxy s7$800The Samsung Galaxy S7 is powered by 1.6GHz octa-core it comes with 4GB 
+of RAM. The phone packs 32GB of internal storage that can be expanded up
+ to 200GB via a microSD card.  Iphone 6 32gb$790It comes with 1GB of RAM. The phone packs 16GB of internal storage 
+cannot be expanded. As far as the cameras are concerned, the Apple 
+iPhone 6 packs a 8-megapixel primary camera on the rear and a 
+1.2-megapixel front shooter for selfies.  Sony xperia z5$320Sony Xperia Z5 Dual smartphone was launched in September 2015. The phone
+ comes with a 5.20-inch touchscreen display with a resolution of 1080 
+pixels by 1920 pixels at a PPI of 424 pixels per inch.  HTC One M9$700The HTC One M9 is powered by 1.5GHz octa-core Qualcomm Snapdragon 810 
+processor and it comes with 3GB of RAM. The phone packs 32GB of internal
+ storage that can be expanded up to 128GB via a microSD card.   Sony vaio i5$790Sony is so confident that the VAIO S is a superior ultraportable laptop 
+that the company proudly compares the notebook to Apple&quot; , &quot;'&quot; , &quot;s 13-inch 
+MacBook Pro. And in a lot of ways this notebook is better, thanks to a 
+lighter weight.  Sony vaio i7
+$790REVIEW
+ 
+Sony is so confident that the VAIO S is a superior 
+ultraportable laptop that the company proudly compares the notebook to 
+Apple&quot; , &quot;'&quot; , &quot;s 13-inch MacBook Pro. And in a lot of ways this notebook is 
+better, thanks to a lighter weight, higher-resolution display, more 
+storage space, and a Blu-ray drive.   &quot;) or . = concat(&quot;
+        Samsung galaxy s6$360The Samsung Galaxy S6 is powered by 1.5GHz octa-core Samsung Exynos 7420
+ processor and it comes with 3GB of RAM. The phone packs 32GB of 
+internal storage cannot be expanded.   Nokia lumia 1520$820The Nokia Lumia 1520 is powered by 2.2GHz quad-core Qualcomm Snapdragon 800 processor and it comes with 2GB of RAM.   Nexus 6$650The Motorola Google Nexus 6 is powered by 2.7GHz quad-core Qualcomm Snapdragon 805 processor and it comes with 3GB of RAM.  Samsung galaxy s7$800The Samsung Galaxy S7 is powered by 1.6GHz octa-core it comes with 4GB 
+of RAM. The phone packs 32GB of internal storage that can be expanded up
+ to 200GB via a microSD card.  Iphone 6 32gb$790It comes with 1GB of RAM. The phone packs 16GB of internal storage 
+cannot be expanded. As far as the cameras are concerned, the Apple 
+iPhone 6 packs a 8-megapixel primary camera on the rear and a 
+1.2-megapixel front shooter for selfies.  Sony xperia z5$320Sony Xperia Z5 Dual smartphone was launched in September 2015. The phone
+ comes with a 5.20-inch touchscreen display with a resolution of 1080 
+pixels by 1920 pixels at a PPI of 424 pixels per inch.  HTC One M9$700The HTC One M9 is powered by 1.5GHz octa-core Qualcomm Snapdragon 810 
+processor and it comes with 3GB of RAM. The phone packs 32GB of internal
+ storage that can be expanded up to 128GB via a microSD card.   Sony vaio i5$790Sony is so confident that the VAIO S is a superior ultraportable laptop 
+that the company proudly compares the notebook to Apple&quot; , &quot;'&quot; , &quot;s 13-inch 
+MacBook Pro. And in a lot of ways this notebook is better, thanks to a 
+lighter weight.  Sony vaio i7
+$790REVIEW
+ 
+Sony is so confident that the VAIO S is a superior 
+ultraportable laptop that the company proudly compares the notebook to 
+Apple&quot; , &quot;'&quot; , &quot;s 13-inch MacBook Pro. And in a lot of ways this notebook is 
+better, thanks to a lighter weight, higher-resolution display, more 
+storage space, and a Blu-ray drive.   &quot;))]</value>
+      <webElementGuid>44b4c3c7-30eb-4c4d-ba38-b0c6dbeccf3f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
